cadd0471303b8333c3568090140083cc4377ad62
Yefferson777 <102541257+Yefferson777@users.noreply.github.com>
2022-05-05 18:27:53 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,115 @@
+
+var express = require('express');
+var router = express.Router();
+
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const database = path.join(__dirname,"database","base.db");
+
+const base = new sqlite3.Database(database, err => {
+
+ if (err)
+  {
+    return console.error(err.message);
+  } 
+  else{
+    console.log("Se creo la Base de Datos");
+  }
+
+});
+
+const tabla = "CREATE TABLE IF NOT EXISTS contactos(nombre VARCHAR(30), gmail VARCHAR(30), comentario TEXT, fecha DATETIME, hora VARCHAR(20), ip VARCHAR(50));";
+
+
+base.run(tabla,err => {
+
+  if (err)
+   {
+     return console.error(err.message);
+   } 
+   else{
+     console.log("Se creo la Tabla");
+   }
+ 
+ });
+
+router.post("/", (req, res) =>{
+
+let datetime = new Date();
+let _date = datetime.toLocaleString();
+let _time = datetime.toLocaleString();
+let date = '';
+let time = '';
+let ip = req.headers['x-forwarded-for'];
+
+for(let d = 0; d <= 8; d++){
+
+  if(_date[d] == '/'){
+    date += '-';
+    continue;
+  }
+  else if(_date[d] == ','){
+      continue;
+    }
+    date += _date[d];
+  }
+
+for(let t = 9; t <= 23; t++){
+
+  if(_time[t] == ' ' || _time[t] == '.' ){
+   break;
+  }  
+
+  time += _time[t];
+}
+
+if (ip){
+  let ip_List = ip.split(',');
+  ip = ip_List[ip_List.length -1];
+}
+else
+{
+  console.log('No se encontro una IP');
+}
+
+  const consulta = "INSERT INTO contactos(nombre, gmail, comentario, fecha, hora, ip) VALUES (?,?,?,?,?,?);";
+  const datos = [req.body.nombre, req.body.gmail, req.body.comentario, date, time, ip];
+
+
+  
+base.run(consulta,datos,err => {
+
+  if (err)
+   {
+     return console.error(err.message);
+   } 
+   else{
+    res.redirect("/contactos"); 
+    console.log("Se contacto con Exito");
+   }
+ 
+ });
+
+});
+
+
+router.get("/contactos", (req, res, next) => {
+
+  const consulta = "SELECT * FROM contactos;";
+  base.all(consulta, [], (err, rows) => {
+
+    if (err)
+     {
+       return console.error(err.message);
+     } 
+     else{
+      res.render("contactos.ejs", {info:rows});  
+     }
+  });
+});
+
+router.get('/',(req,res,next)=>{
+	res.render('index.ejs', {info:{}});
+});
+
+module.exports = router;
