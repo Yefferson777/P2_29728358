4be9104cd3bdc0e5078bed702d57c7cae575aee6
Yefferson777 <102541257+Yefferson777@users.noreply.github.com>
2022-05-05 18:17:41 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-
-app.get('/', (rep, res)=>{
-
-	res.sendFile(path.join(__dirname + "/index.html"));
-});
-app.listen(process.env.PORT || 5555);
